9974d5a1a58aa13bbac8b730b5882d90677462ef
ChangHee Park <arrans9591@gmail.com>
2022-04-05 18:28:01 +0900

    :smile: ass css

--- app.js
+++ app.js
@@ -6,13 +6,26 @@
 
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 // submit 으로 바꿔서 삭제
 // const loginButton = document.querySelector("#login-form button")
 
-const onLoginSubmit = (info) => {
-    info.preventDefault();
-    console.log(loginInput);
+const onLoginSubmit = (event) => {
+    event.preventDefault();
+    const username = loginInput.value;
+
+    // js에서 폼 부분등을 삭제하는 조건
+    // 1. HTML을 삭제한다.
+    // 2. CSS를 이용해서 안보이게 한다.
+
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
+
 }
 
 loginForm.addEventListener("submit" , onLoginSubmit);
