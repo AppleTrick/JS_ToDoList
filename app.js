41cda7be8a92a413e329b1a66a14475b7f3922c4
ChangHee Park <arrans9591@gmail.com>
2022-04-05 17:33:42 +0900

    :whale: preventDefault 사용으로 새로고침 막기

--- app.js
+++ app.js
@@ -4,16 +4,15 @@
 // const loginInput = loginForm.querySelector('input');
 // const loginButton = loginForm.querySelector('button');
 
-const loginInput = document.querySelector("#login-form input")
-const loginButton = document.querySelector("#login-form button")
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-const onLoginBtnClick = () => {
-    const username  = loginInput.value;
-    if(username === ""){
-        alert("Please write your name");
-    }else if(username.length > 15){
-        alert("Yout name is too long");
-    }
+// submit 으로 바꿔서 삭제
+// const loginButton = document.querySelector("#login-form button")
+
+const onLoginSubmit = (info) => {
+    info.preventDefault();
+    console.log(loginInput);
 }
 
-loginButton.addEventListener("click" , onLoginBtnClick)
+loginForm.addEventListener("submit" , onLoginSubmit);
