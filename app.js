45e4c4c75c7cd277e7ca47e4f4923a203db2dad2
ChangHee Park <arrans9591@gmail.com>
2022-04-06 17:01:56 +0900

    :smile: 로그인 폼

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-// const loginForm = document.getElementById("login-form");
-
-// # 1
-// const loginInput = loginForm.querySelector('input');
-// const loginButton = loginForm.querySelector('button');
-
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-
-// submit 으로 바꿔서 삭제
-// const loginButton = document.querySelector("#login-form button")
+const USERNAME_KEY = "username"
 
 const onLoginSubmit = (event) => {
     event.preventDefault();
-    const username = loginInput.value;
-
-    // js에서 폼 부분등을 삭제하는 조건
-    // 1. HTML을 삭제한다.
-    // 2. CSS를 이용해서 안보이게 한다.
-
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-
-
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreeting(username);
 }
 
-loginForm.addEventListener("submit" , onLoginSubmit);
+const paintGreeting = (username) => {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
+
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+
+if(saveUsername === null){
+    // show the Form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit" , onLoginSubmit);
+}else{
+    // show the Greeting
+    paintGreeting(saveUsername);
+}
