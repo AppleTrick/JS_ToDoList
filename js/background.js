9302a298080023b48abbd184262ae6632ef4d368
ChangHee Park <arrans9591@gmail.com>
2022-04-10 18:41:08 +0900

    백그라운드 설정

--- js/background.js
+++ js/background.js
@@ -2,8 +2,8 @@ const images = ["0.png","1.png","2.png"];
 
 const choseImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const bgImage = new Image();
 bgImage.src = `img/${choseImage}`;
-bgImage.width = 1000;
+bgImage.classList.add("bg");
 
 document.body.appendChild(bgImage);
