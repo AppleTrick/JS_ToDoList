41367233e90339baf965c687e88c6f81aeaa1f89
ChangHee Park <arrans9591@gmail.com>
2022-04-12 18:23:21 +0900

    :whale: 시계 완성

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,23 @@
-const clock = document.getElementById("clock");
-
 const getClock = () => {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+
+    const hoursDesign = document.getElementById("hours");
+    const minutesDesign = document.getElementById("minutes");
+    const secondsDesign = document.getElementById("seconds");
+    const sessions = document.getElementById("sessions");
+
+    hoursDesign.innerHTML = hours;
+    minutesDesign.innerHTML = minutes;
+    secondsDesign.innerHTML = seconds;
+
+    if(hours > 12){
+        sessions.innerHTML = "PM";
+    }else{
+        sessions.innerHTML = "AM";
+    }
 }
 
 getClock();
